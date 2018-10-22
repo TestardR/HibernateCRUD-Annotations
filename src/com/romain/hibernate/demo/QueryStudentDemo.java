218557b8275ff679c16ab43d3b4a91a840d2cb30
Romain Testard <40339476+TestardR@users.noreply.github.com>
2018-10-22 20:28:19 +0200

    Add files via upload

---
+++ src/com/romain/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,71 @@
+package com.romain.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.romain.hibernate.demo.entity.Student;
+
+public class QueryStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// query students
+			List<Student> theStudents = session.createQuery("from Student").getResultList();
+			
+			// display the students
+			displayStudents(theStudents);
+			
+			// query students: lastName='Doe'
+			theStudents = session.createQuery("from Student s where s.lastName='Duck'").getResultList();
+			
+			// display the students
+			System.out.println("\n\nStudents who have last name of Duck");
+			displayStudents(theStudents);
+			
+			// query students: lastName='Duck' OR fistName = 'Public'
+			theStudents = 
+					session.createQuery("from Student s where" + " s.lastName='Duck' OR s.firstName='John'").getResultList();
+			
+			System.out.println("\n\nStudents who have last name of Duck OR first name John");
+			displayStudents(theStudents);
+			
+			// query students where email LIKE '%luv2code.com'
+			theStudents = session.createQuery("from Student s where" + " s.email LIKE '%luv2code.com'").getResultList();
+			
+			System.out.println("\n\nStudents who email ends with luv2code.com");
+			displayStudents(theStudents);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+	}
+
+}
